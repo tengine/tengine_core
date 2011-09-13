0f75e74386dcd8a8331d8b5e5d6ef636db08d4dc
t-akima <akima@nautilus-technologies.com>
2011-09-13 14:39:05 +0900

    [tengine_coreパッケージを作成する] ひとまずテストを実行する設定を書いてみました

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,16 @@
+# -*- coding: utf-8 -*-
+ENV["RACK_ENV"] ||= "test" # Mongoid.load!で参照しています
+
+require 'simplecov'
+SimpleCov.start if ENV["COVERAGE"]
+
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'tengine_core'
+
+require 'mongoid'
+Mongoid.load!(File.expand_path('mongoid.yml', File.dirname(__FILE__)))
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
