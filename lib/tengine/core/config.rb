35d33cd9af46f416cc00ac5ef03d44ac194d0458
t-akima <akima@nautilus-technologies.com>
2011-09-15 12:01:41 +0900

    [tengined起動時に-fオプションで設定ファイルを指定した際に、設定ファイルに記載したdb-portの設定が有効でない] バグ修正の前にソースコードを整理しています。Tengine::Core::Config::DEFAULTとそれに関係するクラスメソッドを lib/tengine/core/config.rb に移動しました。

--- lib/tengine/core/config.rb
+++ lib/tengine/core/config.rb
@@ -8,6 +8,8 @@ require 'active_support/ordered_hash'
 require 'active_support/hash_with_indifferent_access'
 require 'active_support/memoizable'
 
+require 'tengine/core/config/default' # DEFAULT定数と関係するメソッドを定義
+
 class Tengine::Core::Config
   autoload :Parser, 'tengine/core/config/parser'
 
@@ -25,27 +27,7 @@ class Tengine::Core::Config
       hash = Tengine::Core::Config::Parser.new(default_hash, args.flatten).parse
       new(hash)
     end
-
-    def default_hash
-      copy_deeply(DEFAULT, {})
-    end
-
-    def copy_deeply(source, dest)
-      source.each do |key, value|
-        case value
-        when NilClass, TrueClass, FalseClass, Numeric, Symbol then
-          dest[key] = value
-        when Hash then
-          dest[key] = copy_deeply(value, dest[key] || {})
-        else
-          dest[key] = value.dup
-        end
-      end
-      dest
-    end
-
   end
-
 
   def initialize(original= nil)
     @hash = ActiveSupport::HashWithIndifferentAccess.new(self.class.default_hash)
@@ -220,84 +202,5 @@ class Tengine::Core::Config
     }.freeze
     @logger_formatters[log_type_name]
   end
-
-
-  public
-
-  # このデフォルト値をdupしたものを、起動時のオプションを格納するツリーとして使用します
-  DEFAULT = {
-    :action => "start", # 設定ファイルには記述しない
-    :config => nil,     # 設定ファイルには記述しない
-
-    :tengined => {
-      :daemon => false,
-      # :prevent_loader    => nil, # デフォルトなし。設定ファイルには記述しない
-      # :prevent_enabler   => nil, # デフォルトなし。設定ファイルには記述しない
-      # :prevent_activator => nil, # デフォルトなし。設定ファイルには記述しない
-      :activation_timeout => 300,
-      # :load_path => "/var/lib/tengine", # 必須
-      :pid_dir        => "./tmp/tengined_pids"       , # 本番環境での例 "/var/run/tengined_pids"
-      :status_dir     => "./tmp/tengined_status"     , # 本番環境での例 "/var/run/tengined_status"
-      :activation_dir => "./tmp/tengined_activations", # 本番環境での例 "/var/run/tengined_activations"
-      :heartbeat_period => 0, # GRハートビートの送信周期。デフォルトではGRハートビートは無効
-      :confirmation_threshold => 'info' # デフォルトではinfo以下のイベントはイベント登録時に自動でconfirmedがtrueになります
-    }.freeze,
-
-    :db => {
-      :host => 'localhost',
-      :port => 27017,
-      :username => nil,
-      :password => nil,
-      :database => 'tengine_production',
-    }.freeze,
-
-    :event_queue => {
-      :connection => {
-        :host => 'localhost',
-        :port => 5672,
-        # :vhost => nil, # デフォルトなし。
-        # :user  => nil, # デフォルトなし。
-        # :pass  => nil, # デフォルトなし。
-      }.freeze,
-      :exchange => {
-        :name => 'tengine_event_exchange',
-        :type => 'direct',
-        :durable => true,
-      }.freeze,
-      :queue => {
-        :name => 'tengine_event_queue',
-        :durable => true,
-      }.freeze,
-    }.freeze,
-
-    :log_common => {
-      :output        => nil        , # file path or "STDOUT" / "STDERR"
-      :rotation      => 3          , # rotation file count or daily,weekly,monthly. default: 3
-      :rotation_size => 1024 * 1024, # number of max log file size. default: 1048576 (10MB)
-      :level         => 'info'     , # debug/info/warn/error/fatal. default: info
-    }.freeze,
-
-    :application_log => {
-      :output        => nil, # file path or "STDOUT" / "STDERR". default: if daemon process then "./log/application.log" else "STDOUT"
-      :rotation      => nil, # rotation file count or daily,weekly,monthly. default: value of --log-common-rotation
-      :rotation_size => nil, # number of max log file size. default: value of --log-common-rotation-size
-      :level         => nil, # debug/info/warn/error/fatal. default: value of --log-common-level
-    }.freeze,
-
-    :process_stdout_log => {
-      :output        => nil, # file path or "STDOUT" / "STDERR". default: if daemon process then "./log/#{$PROGRAM_NAME}_#{Process.pid}_stdout.log" else "STDOUT"
-      :rotation      => nil, # rotation file count or daily,weekly,monthly. default: value of --log-common-rotation
-      :rotation_size => nil, # number of max log file size. default: value of --log-common-rotation-size
-      :level         => nil, # debug/info/warn/error/fatal. default: value of --log-common-level
-    }.freeze,
-
-    :process_stderr_log => {
-      :output        => nil, # file path or "STDOUT" / "STDERR". default: if daemon process then "./log/#{$PROGRAM_NAME}_#{Process.pid}_stderr.log" else "STDERR"
-      :rotation      => nil, # rotation file count or daily,weekly,monthly. default: value of --log-common-rotation
-      :rotation_size => nil, # number of max log file size. default: value of --log-common-rotation-size
-      :level         => nil, # debug/info/warn/error/fatal. default: value of --log-common-level
-    }.freeze,
-
-  }.freeze
 
 end
