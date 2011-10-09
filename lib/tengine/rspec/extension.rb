0cbd71c5bc22abf01005b850cf5092dea01a9481
akima <akima@nautilus-technologies.com>
2011-10-09 23:50:27 +0900

    [tengine_job.gemのドラフト設計・実装] 拡張方法が間違っていたので修正しました

--- lib/tengine/rspec/extension.rb
+++ lib/tengine/rspec/extension.rb
@@ -2,36 +2,42 @@
 # イベントドライバのテストのためのメソッドを追加するモジュールです。
 # includeしてお使いください。
 module Tengine::RSpec::Extension
-  def target_dsl(dsl_path)
-    before do
-      Tengine::Core::Driver.delete_all
-      Tengine::Core::Session.delete_all
-      @__dsl_path__ = File.expand_path("../../app/#{dsl_path}", File.dirname(__FILE__))
-      @__config__ = Tengine::Core::Config.new({
-          :tengined => { :load_path => @__dsl_path__ },
-        })
-      @__bootstrap__ = Tengine::Core::Bootstrap.new(@__config__)
-      @__bootstrap__.load_dsl
-      @__kernel__ = Tengine::Core::Kernel.new(@__config__)
-      @__kernel__.bind
-      @__tengine__ = TengineContextWrapper.new(@__kernel__)
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def target_dsl(dsl_path)
+      before do
+        Tengine::Core::Driver.delete_all
+        Tengine::Core::Session.delete_all
+        @__dsl_path__ = File.expand_path("../../app/#{dsl_path}", File.dirname(__FILE__))
+        @__config__ = Tengine::Core::Config.new({
+            :tengined => { :load_path => @__dsl_path__ },
+          })
+        @__bootstrap__ = Tengine::Core::Bootstrap.new(@__config__)
+        @__bootstrap__.load_dsl
+        @__kernel__ = Tengine::Core::Kernel.new(@__config__)
+        @__kernel__.bind
+        @__tengine__ = TengineContextWrapper.new(@__kernel__)
+      end
+    end
+
+    def driver(driver_name)
+      before do
+        @__driver__ = Tengine::Core::Driver.first(:conditions => {:name => driver_name})
+        session = @__driver__.session
+        @__session__ = Tengine::Core::SessionWrapper.new(session)
+      end
     end
   end
 
-  def driver(driver_name)
-    before do
-      @__driver__ = Tengine::Core::Driver.first(:conditions => {:name => driver_name})
-      session = @__driver__.session
-      @__session__ = Tengine::Core::SessionWrapper.new(session)
+  module InstanceMethods
+    def session
+      @__session__
     end
-  end
 
-  def session
-    @__session__
-  end
-
-  def tengine
-    @__tengine__
+    def tengine
+      @__tengine__
+    end
   end
 
 end
