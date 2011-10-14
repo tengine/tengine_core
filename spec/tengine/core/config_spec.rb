5a6399f0fddb8de5980408ff1401b152fefc9f95
akima <akima@nautilus-technologies.com>
2011-10-14 15:07:51 +0900

    [-Tでファイル名を指定してtenginedを実行するとTengine::Core::Handler not foundというエラーが発生してプロセスが止まる] インデントを整えました

--- spec/tengine/core/config_spec.rb
+++ spec/tengine/core/config_spec.rb
@@ -33,32 +33,32 @@ describe Tengine::Core::Config do
 
 
   shared_examples_for "ディレクトリもファイルも存在しない場合はエラー" do |path|
-      before do
-        @error_message = "file or directory doesn't exist. #{path}"
-        Dir.should_receive(:exist?).with(path).and_return(false)
-        File.should_receive(:exist?).with(path).and_return(false)
-      end
+    before do
+      @error_message = "file or directory doesn't exist. #{path}"
+      Dir.should_receive(:exist?).with(path).and_return(false)
+      File.should_receive(:exist?).with(path).and_return(false)
+    end
 
-      it :dsl_dir_path do
-        expect{ subject.dsl_dir_path }.should raise_error(Tengine::Core::ConfigError, @error_message)
-      end
+    it :dsl_dir_path do
+      expect{ subject.dsl_dir_path }.should raise_error(Tengine::Core::ConfigError, @error_message)
+    end
 
-      it :dsl_file_paths do
-        expect{ subject.dsl_file_paths }.should raise_error(Tengine::Core::ConfigError, @error_message)
-      end
+    it :dsl_file_paths do
+      expect{ subject.dsl_file_paths }.should raise_error(Tengine::Core::ConfigError, @error_message)
+    end
 
-      it :dsl_version_path do
-        expect{ subject.dsl_version_path }.should raise_error(Tengine::Core::ConfigError, @error_message)
-      end
+    it :dsl_version_path do
+      expect{ subject.dsl_version_path }.should raise_error(Tengine::Core::ConfigError, @error_message)
+    end
 
-      it :dsl_version do
-        expect{ subject.dsl_version }.should raise_error(Tengine::Core::ConfigError, @error_message)
-      end
+    it :dsl_version do
+      expect{ subject.dsl_version }.should raise_error(Tengine::Core::ConfigError, @error_message)
+    end
   end
 
   context "load_pathに絶対パス" do
 
-  shared_examples_for "絶対パス指定時のパスの振る舞い" do
+    shared_examples_for "絶対パス指定時のパスの振る舞い" do
       it :dsl_dir_path do
         subject.dsl_dir_path.should == "/var/lib/tengine"
       end
@@ -67,138 +67,138 @@ describe Tengine::Core::Config do
         subject.dsl_version_path.should == "/var/lib/tengine/VERSION"
       end
 
-        it "VERSIONファイルがある場合" do
-          File.should_receive(:exist?).with("/var/lib/tengine/VERSION").and_return(true)
-          File.should_receive(:read).and_return("TEST20110905164100")
-          subject.dsl_version.should == "TEST20110905164100"
+      it "VERSIONファイルがある場合" do
+        File.should_receive(:exist?).with("/var/lib/tengine/VERSION").and_return(true)
+        File.should_receive(:read).and_return("TEST20110905164100")
+        subject.dsl_version.should == "TEST20110905164100"
+      end
+
+      it "VERSIONファイルがない場合" do
+        File.should_receive(:exist?).with("/var/lib/tengine/VERSION").and_return(false)
+        t = Time.local(2011,9,5,17,28,30)
+        Time.stub!(:now).and_return(t)
+        subject.dsl_version.should == "20110905172830"
+      end
+    end
+
+    context "のディレクトリを指定する設定ファイル" do
+      subject do
+        Tengine::Core::Config.new(:config => File.expand_path("config_spec/config_with_dir_absolute_load_path.yml", File.dirname(__FILE__)))
+      end
+      it "should allow to read value by using []" do
+        expected = {
+          'daemon' => true,
+          "activation_timeout" => 300,
+          "load_path" => "/var/lib/tengine",
+          "pid_dir" => "/var/run/tengined_pids",
+          "status_dir" => "/var/run/tengined_status",
+          "activation_dir" => "/var/run/tengined_activations",
+          "heartbeat_period" => 600,
+          "confirmation_threshold" => "warn",
+        }
+        subject[:tengined].should == expected
+        subject['tengined'].should == expected
+        subject[:tengined]['daemon'].should == true
+        subject[:tengined][:daemon].should == true
+        subject[:event_queue][:connection][:host].should == "localhost"
+        subject['event_queue']['connection']['host'].should == "localhost"
+        subject[:event_queue][:queue][:name].should == "tengine_event_queue2"
+        subject['event_queue']['queue']['name'].should == "tengine_event_queue2"
+      end
+      its(:heartbeat_enabled?){ should == true }
+
+      describe :relative_path_from_dsl_dir do
+        it "絶対パスが指定されるとdsl_dir_pathからの相対パスを返します" do
+          Dir.should_receive(:exist?).with("/var/lib/tengine").and_return(true)
+          subject.relative_path_from_dsl_dir("/var/lib/tengine/foo/bar").should == "foo/bar"
         end
 
-        it "VERSIONファイルがない場合" do
-          File.should_receive(:exist?).with("/var/lib/tengine/VERSION").and_return(false)
-          t = Time.local(2011,9,5,17,28,30)
-          Time.stub!(:now).and_return(t)
-          subject.dsl_version.should == "20110905172830"
+        it "相対パスが指定されると（計算のしようがないので）そのまま返します" do
+          subject.relative_path_from_dsl_dir("lib/tengine/foo/bar").should == "lib/tengine/foo/bar"
         end
-  end
-
-  context "のディレクトリを指定する設定ファイル" do
-    subject do
-      Tengine::Core::Config.new(:config => File.expand_path("config_spec/config_with_dir_absolute_load_path.yml", File.dirname(__FILE__)))
-    end
-    it "should allow to read value by using []" do
-      expected = {
-        'daemon' => true,
-        "activation_timeout" => 300,
-        "load_path" => "/var/lib/tengine",
-        "pid_dir" => "/var/run/tengined_pids",
-        "status_dir" => "/var/run/tengined_status",
-        "activation_dir" => "/var/run/tengined_activations",
-        "heartbeat_period" => 600,
-        "confirmation_threshold" => "warn",
-      }
-      subject[:tengined].should == expected
-      subject['tengined'].should == expected
-      subject[:tengined]['daemon'].should == true
-      subject[:tengined][:daemon].should == true
-      subject[:event_queue][:connection][:host].should == "localhost"
-      subject['event_queue']['connection']['host'].should == "localhost"
-      subject[:event_queue][:queue][:name].should == "tengine_event_queue2"
-      subject['event_queue']['queue']['name'].should == "tengine_event_queue2"
-    end
-    its(:heartbeat_enabled?){ should == true }
-
-    describe :relative_path_from_dsl_dir do
-      it "絶対パスが指定されるとdsl_dir_pathからの相対パスを返します" do
-        Dir.should_receive(:exist?).with("/var/lib/tengine").and_return(true)
-        subject.relative_path_from_dsl_dir("/var/lib/tengine/foo/bar").should == "foo/bar"
       end
 
-      it "相対パスが指定されると（計算のしようがないので）そのまま返します" do
-        subject.relative_path_from_dsl_dir("lib/tengine/foo/bar").should == "lib/tengine/foo/bar"
+      describe :confirmation_threshold do
+        it "--tengined-confirmation-levelで設定した値を数値に変換する" do
+          subject.confirmation_threshold.should == Tengine::Event::LEVELS_INV[:warn]
+        end
       end
+
+      context "ディレクトリが存在する場合" do
+        before do
+          Dir.should_receive(:exist?).with("/var/lib/tengine").and_return(true)
+        end
+
+        it :dsl_file_paths do
+          Dir.should_receive(:glob).
+            with("/var/lib/tengine/**/*.rb").
+            and_return(["/var/lib/tengine/foo/bar.rb"])
+          subject.dsl_file_paths.should == ["/var/lib/tengine/foo/bar.rb"]
+        end
+
+        # C0カバレッジを100%にするために追加しています
+        it "dsl_dir_path and dsl_file_paths" do 
+          Dir.should_receive(:glob).
+            with("/var/lib/tengine/**/*.rb").
+            and_return(["/var/lib/tengine/foo/bar.rb"])
+          subject.dsl_dir_path.should == "/var/lib/tengine"
+          subject.dsl_file_paths.should == ["/var/lib/tengine/foo/bar.rb"]
+        end
+
+        it_should_behave_like "絶対パス指定時のパスの振る舞い"
+      end
+
+      it_should_behave_like "ディレクトリもファイルも存在しない場合はエラー", "/var/lib/tengine"
     end
 
-    describe :confirmation_threshold do
-      it "--tengined-confirmation-levelで設定した値を数値に変換する" do
-        subject.confirmation_threshold.should == Tengine::Event::LEVELS_INV[:warn]
+    context "のファイルを指定する設定ファイル" do
+      subject do
+        Tengine::Core::Config.new(:config => File.expand_path("config_spec/config_with_file_absolute_load_path.yml", File.dirname(__FILE__)))
       end
+      it "should allow to read value by using []" do
+        expected = {
+          'daemon' => true,
+          "activation_timeout" => 300,
+          "load_path" => "/var/lib/tengine/init.rb",
+          "pid_dir" => "/var/run/tengined_pids",
+          "status_dir" => "/var/run/tengined_status",
+          "activation_dir" => "/var/run/tengined_activations",
+          "heartbeat_period" => 600,
+          "confirmation_threshold" => "warn",
+        }
+        subject[:tengined].should == expected
+        subject['tengined'].should == expected
+        subject[:tengined]['load_path'].should == "/var/lib/tengine/init.rb"
+        subject[:tengined][:load_path].should == "/var/lib/tengine/init.rb"
+      end
+
+      describe :relative_path_from_dsl_dir do
+        it "絶対パスが指定されるとdsl_dir_pathからの相対パスを返します" do
+          Dir.should_receive(:exist?).with("/var/lib/tengine/init.rb").and_return(false)
+          File.should_receive(:exist?).with("/var/lib/tengine/init.rb").and_return(true)
+          subject.relative_path_from_dsl_dir("/var/lib/tengine/foo/bar").should == "foo/bar"
+        end
+
+        it "相対パスが指定されると（計算のしようがないので）そのまま返します" do
+          subject.relative_path_from_dsl_dir("lib/tengine/foo/bar").should == "lib/tengine/foo/bar"
+        end
+      end
+
+      context "ファイルが存在する場合" do
+        before do
+          Dir.should_receive(:exist?).with("/var/lib/tengine/init.rb").and_return(false)
+          File.should_receive(:exist?).with("/var/lib/tengine/init.rb").and_return(true)
+        end
+
+        it :dsl_file_paths do
+          subject.dsl_file_paths.should == ["/var/lib/tengine/init.rb"]
+        end
+
+        it_should_behave_like "絶対パス指定時のパスの振る舞い"
+      end
+
+      it_should_behave_like "ディレクトリもファイルも存在しない場合はエラー", "/var/lib/tengine/init.rb"
     end
-
-    context "ディレクトリが存在する場合" do
-      before do
-        Dir.should_receive(:exist?).with("/var/lib/tengine").and_return(true)
-      end
-
-      it :dsl_file_paths do
-        Dir.should_receive(:glob).
-          with("/var/lib/tengine/**/*.rb").
-          and_return(["/var/lib/tengine/foo/bar.rb"])
-        subject.dsl_file_paths.should == ["/var/lib/tengine/foo/bar.rb"]
-      end
-
-      # C0カバレッジを100%にするために追加しています
-      it "dsl_dir_path and dsl_file_paths" do 
-        Dir.should_receive(:glob).
-          with("/var/lib/tengine/**/*.rb").
-          and_return(["/var/lib/tengine/foo/bar.rb"])
-        subject.dsl_dir_path.should == "/var/lib/tengine"
-        subject.dsl_file_paths.should == ["/var/lib/tengine/foo/bar.rb"]
-      end
-
-      it_should_behave_like "絶対パス指定時のパスの振る舞い"
-    end
-
-    it_should_behave_like "ディレクトリもファイルも存在しない場合はエラー", "/var/lib/tengine"
-  end
-
-  context "のファイルを指定する設定ファイル" do
-    subject do
-      Tengine::Core::Config.new(:config => File.expand_path("config_spec/config_with_file_absolute_load_path.yml", File.dirname(__FILE__)))
-    end
-    it "should allow to read value by using []" do
-      expected = {
-        'daemon' => true,
-        "activation_timeout" => 300,
-        "load_path" => "/var/lib/tengine/init.rb",
-        "pid_dir" => "/var/run/tengined_pids",
-        "status_dir" => "/var/run/tengined_status",
-        "activation_dir" => "/var/run/tengined_activations",
-        "heartbeat_period" => 600,
-        "confirmation_threshold" => "warn",
-      }
-      subject[:tengined].should == expected
-      subject['tengined'].should == expected
-      subject[:tengined]['load_path'].should == "/var/lib/tengine/init.rb"
-      subject[:tengined][:load_path].should == "/var/lib/tengine/init.rb"
-    end
-
-    describe :relative_path_from_dsl_dir do
-      it "絶対パスが指定されるとdsl_dir_pathからの相対パスを返します" do
-        Dir.should_receive(:exist?).with("/var/lib/tengine/init.rb").and_return(false)
-        File.should_receive(:exist?).with("/var/lib/tengine/init.rb").and_return(true)
-        subject.relative_path_from_dsl_dir("/var/lib/tengine/foo/bar").should == "foo/bar"
-      end
-
-      it "相対パスが指定されると（計算のしようがないので）そのまま返します" do
-        subject.relative_path_from_dsl_dir("lib/tengine/foo/bar").should == "lib/tengine/foo/bar"
-      end
-    end
-
-    context "ファイルが存在する場合" do
-      before do
-        Dir.should_receive(:exist?).with("/var/lib/tengine/init.rb").and_return(false)
-        File.should_receive(:exist?).with("/var/lib/tengine/init.rb").and_return(true)
-      end
-
-      it :dsl_file_paths do
-        subject.dsl_file_paths.should == ["/var/lib/tengine/init.rb"]
-      end
-
-      it_should_behave_like "絶対パス指定時のパスの振る舞い"
-    end
-
-    it_should_behave_like "ディレクトリもファイルも存在しない場合はエラー", "/var/lib/tengine/init.rb"
-  end
   end
 
   context "指定した設定ファイルが存在しない場合" do
