cd01f5f3105248404676804288caf6a2862c7a64
shyouhei <shyouhei@ruby-lang.org>
2011-11-07 21:37:58 +0900

    [tengine_scheduleの実装] tengine_atdの初期実装

---
+++ lib/tengine/core/at.rb
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+require 'daemons'
+require 'eventmachine'
+require 'mongoid'
+require 'uuid'
+
+$LOAD_PATH.push File.expand_path("../../../../lib/", __FILE__)
+
+require 'tengine/core'
+require 'tengine/event'
+require 'tengine/mq'
+
+# explicit loading
+require_relative 'config'
+require_relative 'method_traceable'
+require_relative 'schedule'
+
+class Tengine::Core::At
+
+	def initialize argv
+		@uuid = UUID.new.generate
+		@config = Tengine::Core::Config.parse argv
+		@pid = sprintf "process:%s/%d", ENV["MM_SERVER_NAME"], Process.pid
+	end
+
+	def sender
+		@sender ||= Tengine::Event::Sender.new Tengine::Mq::Suite.new(@config[:event_queue])
+	end
+
+	def send_last_event
+		sender.fire "finished.process.atd.tengine", key: @uuid, source_name: @pid, sender_name: @pid, occurred_at: Time.now, level_key: :info, keep_connection: true
+	rescue Tengine::Event::Sender::RetryError
+		retry # try again
+	else
+		EM.defer lambda { sleep 0.1 until sender.pending_events.empty? }, lambda {|a| EM.stop }
+	end
+
+	def send_periodic_event
+		sender.fire "atd.heartbeat.tengine", key: @uuid, source_name: @pid, sender_name: @pid, occurred_at: Time.now, level_key: :debug, keep_connection: true
+	rescue Tengine::Event::Sender::RetryError
+		# see you next time
+	end
+
+	def send_scheduled_event sched
+		sender.fire sched.event_type_name, source_name: sched.source_name, sender_name: @pid, occurred_at: Time.now, level_key: :info, keep_connection: true
+	rescue Tengine::Event::Sender::RetryError
+		retry # try again
+	end
+
+	def search_for_schedule
+		Tengine::Core::Schedule.where(:scheduled_at.lte => Time.now).each_next_tick do |i|
+			yield i
+		end
+	end
+
+	def shutdown
+		EM.run do
+			EM.cancel_timer @periodic if @periodic
+			send_last_event
+		end
+	end
+
+	def run __file__
+		pdir = File.expand_path @config[:tengined][:pid_dir]
+		fname = File.basename __file__
+		cwd = Dir.getwd
+		Daemons.run_proc fname, ARGV: ['run'], multiple: true, ontop: !@config[:tengined][:daemon], dir_mode: :normal, dir: pdir do
+			Dir.chdir cwd do
+				@config.setup_loggers
+				Tengine::Core::MethodTraceable.disabled = !@config[:verbose]
+				Mongoid::Document.module_eval do
+					include Tengine::Core::CollectionAccessible
+				end
+				Mongoid.config.from_hash @config[:db]
+				Mongoid.config.option :persist_in_safe_mode, default: true
+				EM.run do
+					sender.wait_for_connection do
+						@invalidate = EM.add_periodic_timer 1 do # !!! MAGIC NUMBER
+							search_for_schedule do |sched|
+								send_scheduled_event sched
+							end
+						end
+						int = @config[:heartbeat][:hbw][:interval]
+						if int and int > 0
+							@periodic = EM.add_periodic_timer int do
+								send_periodic_event
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+
+	extend Tengine::Core::MethodTraceable
+	method_trace(*instance_methods(false))
+end
+
+# 
+# Local Variables:
+# mode: ruby
+# coding: utf-8-unix
+# indent-tabs-mode: t
+# tab-width: 3
+# ruby-indent-level: 3
+# fill-column: 79
+# default-justification: full
+# End:
