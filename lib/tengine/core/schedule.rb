bc524bba0504360487d39996bd07ab79b9e7dc0c
shyouhei <shyouhei@ruby-lang.org>
2011-11-08 19:41:47 +0900

    [tengine_scheduleの実装] デフォルトを設定しました

--- lib/tengine/core/schedule.rb
+++ lib/tengine/core/schedule.rb
@@ -5,8 +5,13 @@ class Tengine::Core::Schedule
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  # statuses
+  SCHEDULED = 0
+  INVALID   = 1
+  FIRED     = 2
+
   field :event_type_name, :type => String
-  field :scheduled_at   , :type => Time
-  field :status         , :type => Integer
+  field :scheduled_at   , :type => Time   , :default => proc { Time.now } 
+  field :status         , :type => Integer, :default => SCHEDULED
   field :source_name    , :type => String
 end
