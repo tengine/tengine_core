913bcdfb0d61f85e5036c05af3c6e26831a812f0
shyouhei <shyouhei@ruby-lang.org>
2011-11-14 13:22:19 +0900

    インデントの変更

--- lib/tengine/core/heartbeat_watcher.rb
+++ lib/tengine/core/heartbeat_watcher.rb
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- :coding => utf-8 -*-
 require 'daemons'
 require 'eventmachine'
 require 'mongoid'
@@ -16,121 +16,121 @@ require_relative 'method_traceable'
 
 class Tengine::Core::HeartbeatWatcher
 
-	def initialize argv
-		@uuid = UUID.new.generate
-		@config = Tengine::Core::Config.parse argv
-		@pid = sprintf "process:%s/%d", ENV["MM_SERVER_NAME"], Process.pid
-	end
+  def initialize argv
+    @uuid = UUID.new.generate
+    @config = Tengine::Core::Config.parse argv
+    @pid = sprintf "process:%s/%d", ENV["MM_SERVER_NAME"], Process.pid
+  end
 
-	def sender
-		@sender ||= Tengine::Event::Sender.new Tengine::Mq::Suite.new(@config[:event_queue])
-	end
+  def sender
+    @sender ||= Tengine::Event::Sender.new Tengine::Mq::Suite.new(@config[:event_queue])
+  end
 
-	def send_last_event
-		sender.fire "finished.process.hbw.tengine", key: @uuid, source_name: @pid, sender_name: @pid, occurred_at: Time.now, level_key: :info, keep_connection: true
-	rescue Tengine::Event::Sender::RetryError
-		retry # try again
-	else
-		p = proc do
-			EM.next_tick do
-				if sender.pending_events.empty?
-					sender.mq_suite.connection.close do
-						EM.stop
-					end
-				else
-					p.call
-				end
-			end
-		end
-		p.call
-	end
+  def send_last_event
+    sender.fire "finished.process.hbw.tengine", :key => @uuid, :source_name => @pid, :sender_name => @pid, :occurred_at => Time.now, :level_key => :info, :keep_connection => true
+  rescue Tengine::Event::Sender::RetryError
+    retry # try again
+  else
+    p = proc do
+      EM.next_tick do
+        if sender.pending_events.empty?
+          sender.mq_suite.connection.close do
+            EM.stop
+          end
+        else
+          p.call
+        end
+      end
+    end
+    p.call
+  end
 
-	def send_periodic_event
-		sender.fire "hbw.heartbeat.tengine", key: @uuid, source_name: @pid, sender_name: @pid, occurred_at: Time.now, level_key: :debug, keep_connection: true
-	rescue Tengine::Event::Sender::RetryError
-		# see you next time
-	end
+  def send_periodic_event
+    sender.fire "hbw.heartbeat.tengine", :key => @uuid, :source_name => @pid, :sender_name => @pid, :occurred_at => Time.now, :level_key => :debug, :keep_connection => true
+  rescue Tengine::Event::Sender::RetryError
+    # see you next time
+  end
 
-	def send_invalidate_event type, e0
-		obj = e0.as_document.to_hash.inject({}) {|r, (k, v)| r.update(k.to_sym => v) }
-		Tengine.logger.info "Heartbeat expiration detected! for #{e0.event_type_name} of #{e0.source_name}: last seen #{e0.occurred_at} (#{(Time.now - e0.occurred_at).to_f} secs before)"
-		obj.delete :_id
-		obj.delete :confirmed
-		obj.delete :updated_at
-		obj.delete :created_at
-		obj[:event_type_name] = type
-		e1 = Tengine::Event.new obj
-		sender.fire e1, keep_connection: true
-	rescue Tengine::Event::Sender::RetryError
-		# see you next time
-	end
+  def send_invalidate_event type, e0
+    obj = e0.as_document.to_hash.inject({}) {|r, (k, v)| r.update(k.to_sym => v) }
+    Tengine.logger.info "Heartbeat expiration detected! for #{e0.event_type_name} of #{e0.source_name}: last seen #{e0.occurred_at} (#{(Time.now - e0.occurred_at).to_f} secs before)"
+    obj.delete :_id
+    obj.delete :confirmed
+    obj.delete :updated_at
+    obj.delete :created_at
+    obj[:event_type_name] = type
+    e1 = Tengine::Event.new obj
+    sender.fire e1, :keep_connection => true
+  rescue Tengine::Event::Sender::RetryError
+    # see you next time
+  end
 
-	def search_for_invalid_heartbeat
-		t = Time.now
-		a = @config[:heartbeat].each_pair.map do |e, h|
-			Tengine::Core::Event.where(
-				event_type_name: "#{e}.heartbeat.tengine",
-				:occurred_at.lte => t - h[:expire]
-			)
-		end
-		a.flatten.each_next_tick do |i|
-			yield i if i
-		end
-	end
+  def search_for_invalid_heartbeat
+    t = Time.now
+    a = @config[:heartbeat].each_pair.map do |e, h|
+      Tengine::Core::Event.where(
+                                 :event_type_name => "#{e}.heartbeat.tengine",
+                                 :occurred_at.lte => t - h[:expire]
+                                 )
+    end
+    a.flatten.each_next_tick do |i|
+      yield i if i
+    end
+  end
 
-	def shutdown
-		EM.run do
-			EM.cancel_timer @invalidate if @invalidate
-			EM.cancel_timer @periodic if @periodic
-			send_last_event
-		end
-	end
+  def shutdown
+    EM.run do
+      EM.cancel_timer @invalidate if @invalidate
+      EM.cancel_timer @periodic if @periodic
+      send_last_event
+    end
+  end
 
-	def run __file__
-		pdir = File.expand_path @config[:tengined][:pid_dir]
-		fname = File.basename __file__
-		cwd = Dir.getwd
-		Daemons.run_proc fname, ARGV: ['run'], multiple: true, ontop: !@config[:tengined][:daemon], dir_mode: :normal, dir: pdir do
-			Dir.chdir cwd do
-				@config.setup_loggers
-				Tengine::Core::MethodTraceable.disabled = !@config[:verbose]
-				Mongoid.config.from_hash @config[:db]
-				Mongoid.config.option :persist_in_safe_mode, default: true
-				EM.run do
-					sender.wait_for_connection do
-						@invalidate = EM.add_periodic_timer 1 do # !!! MAGIC NUMBER
-							search_for_invalid_heartbeat do |obj|
-								type = case obj.event_type_name when /job|core|hbw/ then
-											 "expired.#$&.heartbeat.tengine"
-										 end
-								EM.next_tick do
-									send_invalidate_event type, obj
-								end
-							end
-						end
-						int = @config[:heartbeat][:hbw][:interval]
-						if int and int > 0
-							@periodic = EM.add_periodic_timer int do
-								send_periodic_event
-							end
-						end
-					end
-				end
-			end
-		end
-	end
+  def run __file__
+    pdir = File.expand_path @config[:tengined][:pid_dir]
+    fname = File.basename __file__
+    cwd = Dir.getwd
+    Daemons.run_proc fname, :ARGV => ['run'], :multiple => true, :ontop => !@config[:tengined][:daemon], :dir_mode => :normal, :dir => pdir do
+      Dir.chdir cwd do
+        @config.setup_loggers
+        Tengine::Core::MethodTraceable.disabled = !@config[:verbose]
+        Mongoid.config.from_hash @config[:db]
+        Mongoid.config.option :persist_in_safe_mode, :default => true
+        EM.run do
+          sender.wait_for_connection do
+            @invalidate = EM.add_periodic_timer 1 do # !!! MAGIC NUMBER
+              search_for_invalid_heartbeat do |obj|
+                type = case obj.event_type_name when /job|core|hbw/ then
+                         "expired.#$&.heartbeat.tengine"
+                       end
+                EM.next_tick do
+                  send_invalidate_event type, obj
+                end
+              end
+            end
+            int = @config[:heartbeat][:hbw][:interval]
+            if int and int > 0
+              @periodic = EM.add_periodic_timer int do
+                send_periodic_event
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 
-	extend Tengine::Core::MethodTraceable
-	method_trace(*instance_methods(false))
+  extend Tengine::Core::MethodTraceable
+  method_trace(*instance_methods(false))
 end
 
 # 
 # Local Variables:
 # mode: ruby
 # coding: utf-8-unix
-# indent-tabs-mode: t
-# tab-width: 3
-# ruby-indent-level: 3
+# indent-tabs-mode: nil
+# tab-width: 4
+# ruby-indent-level: 2
 # fill-column: 79
 # default-justification: full
 # End:
