294545a905d5f96602acffd85c0dbb1078ad03a7
akima <akima@nautilus-technologies.com>
2011-12-07 20:13:42 +0900

    [tenginedを起動するたびに、ドライバ、ハンドラパス、ジョブなどが登録されてしまう] DSLを評価するコンテキストから使用されないメソッドをTengine::Core::DslLoaderから削除しました

--- lib/tengine/core/dsl_loader.rb
+++ lib/tengine/core/dsl_loader.rb
@@ -87,47 +87,10 @@ module Tengine::Core::DslLoader
     end
   end
 
-  # イベントドライバにイベントハンドラを登録します。
-  #
-  # このメソッドは、driverメソッドに渡されたブロックの中で使用する必要があります。
-  #
-  # @param [String/Symbol/Tengine::Core::DslFilterDef] filter_def ハンドリングするイベント種別名、あるいはそれらの組み合わせ。
-  # @return [Tengine::Core::Handler]
-  # @see #driver
-  #
-  # filter_defとして複合した条件を記述することも可能です。
-  # {include:file:examples/uc08_if_both_a_and_b_occurs.rb}
-  def on(filter_def, options = {}, &block)
-    unless block_given?
-      # Tengine::Core::stdout.info("no block given at #{caller.first}")
-      return
-    end
-    event_type_names = filter_def.respond_to?(:event_type_names) ? filter_def.event_type_names : [filter_def.to_s]
-    filepath, lineno = *__source_location__(block)
-    @__driver__.handlers.new(
-      # filepathはTengineコアが動く環境ごとに違うかもしれないので、相対パスを使う必要があります。
-      :filepath => config.relative_path_from_dsl_dir(filepath),
-      :lineno => lineno,
-      :event_type_names => event_type_names,
-      :filter => filter_def.is_a?(Tengine::Core::DslFilterDef) ? filter_def.filter : nil)
-    # 一つのドライバに対して複数個のハンドラを登録しようとした際に警告を出すべきだが・・・
-    # Tengine::Core::stdout.warn("driver#{@__driver__.name.dump}には、同一のevent_type_name#{event_type_name.to_s.dump}が複数存在します")
-  end
-
   # @see Tengine::Core::DslBinder#session
   def session
     raise Tengine::Core::DslError, "session is not available outside of event driver block." unless @__session__
     @__session_wrapper__ ||= Tengine::Core::SessionWrapper.new(@__session__)
-  end
-
-  # @see Tengine::Core::DslBinder#event
-  def event
-    raise Tengine::Core::DslError, "event is not available outside of event handler block."
-  end
-
-  # @see Tengine::Core::DslBinder#submit
-  def submit
-    raise Tengine::Core::DslError, "submit is not available outside of event handler block."
   end
 
 
