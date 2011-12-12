f6b05a98bd941432b79cd9574a40fcd89c562e49
shyouhei <shyouhei@ruby-lang.org>
2011-12-12 16:46:57 +0900

    このspecは誤りです

--- spec/tengine/core_spec.rb
+++ spec/tengine/core_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe Tengine::Core do
   describe :version do
     it "３つの数値をドットで区切った文字列" do
-      Tengine::Core.version.should =~ /^\d+\.\d+\.\d+$/
+      Tengine::Core.version.should =~ /^\d+\.\d+\.\d+/
     end
     it "プロジェクトのルートにあるVERSIONファイルの内容と同じです" do
       Tengine::Core.version.should == File.read(File.expand_path("../../VERSION", File.dirname(__FILE__))).strip
