3124cd38c68cc064e40b54cffb581efaa3347312
akima <akima@nautilus-technologies.com>
2011-12-26 18:41:18 +0900

    [tengindのプロセスを二つ起動した際に並列ジョブがある際にジョブが２度実行される] Tengine::Job::RootJobnetActualのフィールドlock_versionを単にversionに変更したいので、ActiveRecord::Locking::Optimisticと同じようにロックに使用する属性名を変更できるようにしました

--- lib/tengine/core/optimistic_lock.rb
+++ lib/tengine/core/optimistic_lock.rb
@@ -1,8 +1,19 @@
+# -*- coding: utf-8 -*-
 require 'tengine/core'
 
 require 'active_support/core_ext/array/extract_options'
 
 module Tengine::Core::OptimisticLock
+  extend ActiveSupport::Concern
+
+  included do
+    cattr_accessor :lock_optimistically, :instance_writer => false
+    self.lock_optimistically = true
+
+    class << self
+      alias_method :locking_field=, :set_locking_field
+    end
+  end
 
   class RetryOverError < StandardError
   end
@@ -20,13 +31,39 @@ module Tengine::Core::OptimisticLock
   end
 
   def __find_and_modify__
-    current_version = self.lock_version
+    lock_field_name = self.class.locking_field
+    current_version = self.send(lock_field_name)
     hash = as_document.dup
-    hash['lock_version'] = current_version + 1
+    new_version = current_version + 1
+    hash[lock_field_name] = new_version
     result = self.class.collection.find_and_modify({
-        :query => {:_id => self.id, :lock_version => current_version},
+        :query => {:_id => self.id, lock_field_name.to_sym => current_version},
         :update => hash
       })
+    send("#{lock_field_name}=", new_version)
     result
   end
+
+  # ActiveRecord::Locking::Optimistic::ClassMethods を参考に実装しています
+  # https://github.com/rails/rails/blob/master/activerecord/lib/active_record/locking/optimistic.rb
+  module ClassMethods
+    DEFAULT_LOCKING_FIELD = 'lock_version'.freeze
+
+    # Set the field to use for optimistic locking. Defaults to +lock_version+.
+    def set_locking_field(value = nil, &block)
+      define_attr_method :locking_field, value, &block
+      value
+    end
+
+    # The version field used for optimistic locking. Defaults to +lock_version+.
+    def locking_field
+      reset_locking_field
+    end
+
+    # Reset the field used for optimistic locking back to the +lock_version+ default.
+    def reset_locking_field
+      set_locking_field DEFAULT_LOCKING_FIELD
+    end
+  end
+
 end
