02c93e7793f9938b81326dbc4e146dbcc69d01c1
akima <akima@nautilus-technologies.com>
2012-01-06 18:53:30 +0900

    [各コレクションに必要なインデックスをモデルに定義する] findメソッドについてはexplainを出力するように変更しました

--- spec/support/mongo_index_key_log.rb
+++ spec/support/mongo_index_key_log.rb
@@ -15,7 +15,7 @@
 # cat mongo_index_key.log | sort | uniq > mongo_index_key_summary.log
 #
 # これで $HOME/tmp/mongo_index_key_summary.log にどのようなキーが使われているのかがまとめられます
-# 
+#
 if ENV['MONGO_INDEX_KEY_LOG']
   file_path = File.expand_path(ENV['MONGO_INDEX_KEY_LOG'])
   STDOUT.puts("MONGO_INDEX_KEY_LOG enable logging key infomation for index to #{file_path}")
@@ -30,8 +30,9 @@ if ENV['MONGO_INDEX_KEY_LOG']
     end
 
     def find_with_index_key_log(selector={}, opts={}, &block)
-      index_key_log_file.puts("#{self.name}.find({#{_hash_keys_(selector)}}, {#{_hash_keys_(opts.dup)}})")
-      find_without_index_key_log(selector, opts, &block)
+      result = find_without_index_key_log(selector, opts, &block)
+      index_key_log_file.puts("#{self.name}.find({#{_hash_keys_(selector)}}, {#{_hash_keys_(opts.dup)}}): #{_explain_summary_(result.explain)}")
+      result
     end
     alias_method :find_without_index_key_log, :find
     alias_method :find, :find_with_index_key_log
@@ -48,14 +49,41 @@ if ENV['MONGO_INDEX_KEY_LOG']
 
     def _hash_keys_(hash)
       res = []
-      if sort = hash.delete(:sort)
-        res << ":sort => #{sort.inspect}"
-      end
-      if hint = hash.delete(:hint)
-        res << ":hint => #{hint.inspect}"
+      [:sort, :hint, :limit].each do |key|
+        if value = hash.delete(key)
+          res << "#{key.inspect} => #{value.inspect}"
+        end
       end
       res += hash.keys.map(&:inspect)
       res.join(',')
+    end
+
+    def _explain_summary_(hash)
+      case cursor = hash['cursor']
+      when /^BtreeCursor/ then
+        res = []
+        %w[cursor].each do |key|
+          if value = hash.delete(key)
+            res << "#{key.inspect} => #{value.inspect}"
+          end
+        end
+        res += hash.keys.map(&:inspect)
+        "{" << res.join(',') << "}"
+      else
+        res = []
+        res << "'cursor' => #{hash.delete('cursor').inspect}"
+        {"nscanned" => 1, "nscannedObjects" => 1, "n" => 1, "millis" => 0, "nYields"=>0, "nChunkSkips"=>0 }.each do |key, border|
+          if value = hash.delete(key)
+            if value > border
+              res << "#{key.inspect} => '>#{border.inspect}'"
+            else
+              res << "#{key.inspect} => #{value.inspect}"
+            end
+          end
+        end
+        res += hash.keys.map(&:inspect)
+        "{" << res.join(',') << "}"
+      end
     end
 
   end
