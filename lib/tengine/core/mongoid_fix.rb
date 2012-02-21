57ddf0a006b7b60dd6507e8b62072e5fa45d3fc7
Urabe, Shyouhei <shyouhei@ruby-lang.org>
2012-02-21 23:33:02 +0900

    https://github.com/mongoid/mongoid/pull/1739 暫定対応です

--- lib/tengine/core/mongoid_fix.rb
+++ lib/tengine/core/mongoid_fix.rb
@@ -4,14 +4,31 @@ require 'mongoid'
 require 'mongoid/version'
 require 'mongoid/cursor'
 
-if Mongoid::VERSION <= "3.0.0"
+if Mongoid::VERSION < "3.0.0"
   class Mongoid::Cursor
     # https://github.com/mongoid/mongoid/pull/1609
     def each
-      loop do
-        retry_on_connection_failure do
-          return unless document = cursor.next
+      retry_on_connection_failure do
+        while document = cursor.next
           yield Mongoid::Factory.from_db(klass, document)
+        end
+      end
+    end
+
+    # https://github.com/mongoid/mongoid/pull/1739
+    def retry_on_connection_failure
+      retries = 0
+      begin
+        yield
+      rescue Mongo::ConnectionFailure, Mongo::OperationTimeout => ex
+        retries = increase_retry_attempts(retries, ex)
+        retry
+      rescue Mongo::OperationFailure => ex
+        if ex.message =~ /not master/
+          retries = increase_retry_attempts(retries, ex)
+          retry
+        else
+          raise ex
         end
       end
     end
